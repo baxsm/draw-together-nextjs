20c7440ec00827d001b64dc6adb11f973ed043f0
Bobby Aleem <bobbybeatmaker@hotmail.com>
2023-12-18 14:04:48 +0500

    chore: basic readme and vercel analytics

---
+++ README.md
@@ -0,0 +1,1 @@
+Live | https://draw-together-nextjs.vercel.app/
--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "@/components/Providers";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
   );
